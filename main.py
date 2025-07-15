aa5bdbbe5bda9e03921f734e2fcbf1d388657672
Ixodes <ixotech1@gmail.com>
2025-07-15 17:49:40 +0900

    Completed L6:Count Characters

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from stats import get_num_words
+from stats import get_num_chars
 
 def main():
     filepath="books/frankenstein.txt"
     print(get_num_words(filepath))
+    print(get_num_chars(filepath))
 
 main()
--- stats.py
+++ stats.py
@@ -6,3 +6,11 @@ def get_num_words(filepath):
     num_words=len(get_book_text(filepath).split())
     return f"{num_words} words found in the document"
 
+def get_num_chars(filepath):
+    res={}
+    for x in get_book_text(filepath).lower():
+        if x in res:
+            res[x]+=1
+        else:
+            res[x]=1
+    return res
