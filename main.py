dfb0f4c2ba10c2bb294d123b7491c88a3d098a2d
Ixodes <ixotech1@gmail.com>
2025-07-16 10:08:24 +0900

    Completed L2:Arguments

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+import sys
 from stats import get_num_words
 from stats import get_num_chars
 from stats import sorted_num_chars
 
+if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
 def main():
-    filepath="books/frankenstein.txt"
+    filepath=sys.argv[1]
     print("Word Count")
     print(get_num_words(filepath))
     chars=get_num_chars(filepath)
